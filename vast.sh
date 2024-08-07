2f599c2fbec9354e3c59b44a106724f6454d6be3
4pii4 <138131197+4pii4@users.noreply.github.com>
2024-08-07 10:22:58 +0700

    zzz

--- vast.sh
+++ vast.sh
@@ -12,8 +12,8 @@ pipx install bpytop nvitop
 release=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest)
 deb=$(echo "$release" | jq -r '.assets[] | select(.name == "cloudflared-linux-amd64.deb") | .browser_download_url')
 wget -O cloudflared-linux-amd64.deb "$deb"
-sudo dpkg -i cloudflared-linux-amd64.deb
+dpkg -i cloudflared-linux-amd64.deb
 rm cloudflared-linux-amd64.deb
 
-grep -q '~/.local/bin' && echo 'PATH="$PATH:~/.local/bin"' >> ~/.bashrc
+grep -q '~/.local/bin' ~/.bashrc && echo 'PATH="$PATH:~/.local/bin"' >> ~/.bashrc
 source ~/.bashrc
