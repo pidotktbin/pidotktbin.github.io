bb0edbc2cf13151c8b2a704ab0e8141afeea073f
4pii4 <138131197+4pii4@users.noreply.github.com>
2024-08-07 10:20:19 +0700

    my ass

--- vast.sh
+++ vast.sh
@@ -4,17 +4,16 @@ curl https://codeberg.org/segv/dotfiles/raw/branch/master/.tmux.conf -Ss > ~/.tm
 tmux source-file ~/.tmux.conf
 apt update
 apt upgrade -y
-apt-get install python3-dev python3-venv python3-pip htop neovim ninja-build -y
+apt-get install python3-dev python3-venv python3-pip htop neovim ninja-build jq -y
 apt-get install ffmpeg libsm6 libxext6  -y
 pip install pipx
 pipx install bpytop nvitop
 
-release=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest); 
-deb=$(echo "$release" | jq -r '.assets[] | select(.name | endswith("amd64.deb")) | .browser_download_url'); 
-wget -O cloudflared_amd64.deb "$deb"; 
-dpkg -i cloudflared_amd64.deb; 
-rm cloudflared_amd64.deb
+release=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest)
+deb=$(echo "$release" | jq -r '.assets[] | select(.name == "cloudflared-linux-amd64.deb") | .browser_download_url')
+wget -O cloudflared-linux-amd64.deb "$deb"
+sudo dpkg -i cloudflared-linux-amd64.deb
+rm cloudflared-linux-amd64.deb
 
-
-echo 'PATH="$PATH:~/.local/bin"' >> ~/.bashrc
+grep -q '~/.local/bin' && echo 'PATH="$PATH:~/.local/bin"' >> ~/.bashrc
 source ~/.bashrc
